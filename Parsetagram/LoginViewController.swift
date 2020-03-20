ad6561388ca2407bab945c9dee693a46536a23cc
Nada Zeini <nada.elzeini@sjsu.edu>
2020-03-19 23:23:30 -0700

    sign in works

---
+++ Parsetagram/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Parsetagram
+//
+//  Created by Nada Zeini on 3/19/20.
+//  Copyright © 2020 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+import Parse
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var signUp: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+           else {
+                print("error")
+            }
+        }
+    }
+}
